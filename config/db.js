15f9565c05c4344289d1f0f384089ff72c0b2e15
Gerardo Luis Arrazola Mariscal <arrazola267@gmail.com>
2025-11-18 10:09:09 -0400

    cambios

--- config/db.js
+++ config/db.js
@@ -4,3 +4,6 @@ export const SUPABASE_URL = 'https://dzatmxvwmpczteaqpmmm.supabase.co';
 export const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6YXRteHZ3bXBjenRlYXFwbW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MjY3OTAsImV4cCI6MjA3NjEwMjc5MH0.8Xf6Mx6DzJ4tGSO-VlisiBlUpgC4XxmAdNRf6j3afAs';                 // <- pega aquí tu anon key (no uses service_role)
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
+
+
+window.supabase = supabase; // ← permitir usar supabase en consola
